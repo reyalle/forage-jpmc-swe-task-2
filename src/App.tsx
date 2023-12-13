1c2fb00cdf98eb0bbf0a29c783a466c461d04f60
reyalle <reyxeka@gmail.com>
2023-12-13 16:41:54 -0500

    Made changes to recieve data continously

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ import './App.css';
 /**
  * State declaration for <App />
  */
+// Added showGraph property and set it to boolean type
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -21,26 +23,40 @@ class App extends Component<{}, IState> {
     this.state = {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
+      // Set default state of showGraph to false so it can be hidden until button click
       data: [],
+      showGraph: false,
     };
   }
 
   /**
    * Render Graph react component with state.data parse as property data
    */
+  // Added condition to only render graph once the showGraph property is set to true
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
+  // Modified getDataFromServer function to get data continuously instead of on click
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    let x = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x > 1000) {
+        clearInterval(interval)
+      }
+    }, 100);
   }
 
   /**
